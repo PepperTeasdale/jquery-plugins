edd61b187b66fe035a51b08b358818d9bf675a17
Justin and Pat <PatandJustin@appacademy.io>
2015-11-05 12:00:20 -0500

    Implement tab switching.

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -1,7 +1,23 @@
-$.Tabs = function (el) { ... };
+$.Tabs = function (el) {
+  this.$el = $(el);
+  this.$contentTabs = $(this.$el.attr("data-content-tabs"));
+  this.$activeTab = $(this.$el.find(".active"));
+  this.$el.on("click", "a", function (event) {
+    event.preventDefault();
+    this.clickTab(event);
+  }.bind(this))
+};
 
 $.fn.tabs = function () {
   return this.each(function () {
     new $.Tabs(this);
   });
 };
+
+$.Tabs.prototype.clickTab = function(event) {
+  this.$activeTab.removeClass("active");
+  this.$contentTabs.children().removeClass("active");
+  this.$activeTab = $(event.currentTarget);
+  this.$activeTab.addClass("active");
+  $(this.$activeTab.attr("href")).addClass("active");
+}
