6199360dcbe6310bd6a0add4d45955c8ecbda50c
Justin and Pat <PatandJustin@appacademy.io>
2015-11-05 16:18:01 -0500

    Implement hovering preview.

--- thumbnails/js/thumbnails.js
+++ thumbnails/js/thumbnails.js
@@ -2,6 +2,19 @@ $.Thumbnails = function (el) {
   this.$el = $(el);
   this.$activeImg = $(this.$el.find(".gutter-images").children()[0]);
   this.activate(this.$activeImg);
+
+  this.$el.on("click",".gutter-images img", function(event) {
+    this.activate($(event.currentTarget));
+    this.$activeImg = $(event.currentTarget);
+  }.bind(this));
+
+  this.$el.on("mouseenter", ".gutter-images img", function(event) {
+    this.activate($(event.currentTarget));
+  }.bind(this));
+
+  this.$el.on("mouseleave", ".gutter-images img", function(event) {
+    this.activate(this.$activeImg);
+  }.bind(this));
 };
 
 $.fn.thumbnails = function () {
@@ -11,10 +24,8 @@ $.fn.thumbnails = function () {
 };
 
 $.Thumbnails.prototype.activate = function($img) {
-  // debugger;
+  this.$el.find(".active").empty();
   var $newActive = $("<img>");
   $newActive.attr("src", ($img.attr("src")));
   this.$el.find(".active").append($newActive);
-
-  this.$activeImg = $img;
 }
